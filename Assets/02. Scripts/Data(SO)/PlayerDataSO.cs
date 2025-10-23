3cd96a9167cea63753c8092691e6e9b965d4ddb9
phs970510-droid <phs970510@gmail.com>
2025-10-23 19:02:42 +0900

    script
    playerdata
    playercontroller

---
+++ Assets/02. Scripts/Data(SO)/PlayerDataSO.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="PlayerData",menuName ="Game/Player Data")]
+public class PlayerDataSO : ScriptableObject
+{
+    [Header("기본 스텟")]
+    public float maxHP = 100.0f;
+    public float moveSpeed = 5.0f;
+
+    [Header("무기 보정치(성장용)")]
+    public float damageBonus = 0.0f;
+    public float reloadSpeedBonus = 0.0f;
+    public float fireRateBonus = 0.0f;
+
+    [Header("기타 추가사항")]
+    [HideInInspector] public float currentHP;
+
+    private void OnEnable()
+    {
+        currentHP = maxHP;
+    }
+    public void UpgradeHP(float value)
+    {
+        maxHP += value;
+        currentHP = maxHP;
+    }
+    public void UpgradeMoveSpeed(float value) 
+    { 
+        moveSpeed += value;
+    }
+    public void UpgradeDamage(float value)
+    { 
+        damageBonus += value; 
+    }
+    public void UpgradeReloadSpeed(float value)
+    { 
+        reloadSpeedBonus += value; 
+    }
+    public void UpgradeFireRate(float value)
+    { 
+        fireRateBonus += value; 
+    }
+}
--- Assets/02. Scripts/Player/PlayerController.cs
+++ Assets/02. Scripts/Player/PlayerController.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("참조")]
+    public PlayerDataSO playerData; //값 주입받기
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    { 
+        rb.MovePosition(rb.position+moveInput.normalized*playerData.moveSpeed*Time.fixedDeltaTime);
     }
 }
